1c0e43dab14b521c99a1369edc399d208e7c2f1d
AriadneClarissa <ariadneclarissadarnettaoswari_2327240056@mhs.mdp.ac.id>
2026-04-15 14:53:40 +0700

    main dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,5 +9,21 @@ void main()async{
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
-  
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Employee App',
+      theme: ThemeData(
+        primarySwatch: Color.blue,
+        useMaterial3: true,
+      ),
+      home: const AddEmployeeScreen(),
+    );
+  }
 }
